9f516f1cccf9a2ce74468e678b4418caa8f0153e
unknown <smritieligar1005@gmail.com>
2025-05-18 13:02:24 +0530

    dockerized the application

--- api/main.py
+++ api/main.py
@@ -1,65 +1,61 @@
-from fastapi import FastAPI, HTTPException
+import traceback
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 import qrcode
 import boto3
 import os
 from io import BytesIO
-
-# Loading Environment variable (AWS Access Key and Secret Key)
 from dotenv import load_dotenv
+import re
+
 load_dotenv()
 
 app = FastAPI()
 
-# Allowing CORS for local testing
-origins = [
-    "http://localhost:3000"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
+    #  allow_origins=[
+    #     "http://localhost:3000",
+    #     "http://localhost:3001",  
+    # ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# AWS S3 Configuration
 s3 = boto3.client(
     's3',
-    aws_access_key_id= os.getenv("AWS_ACCESS_KEY"),
-    aws_secret_access_key= os.getenv("AWS_SECRET_KEY"))
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_KEY")
+)
 
-bucket_name = 'YOUR_BUCKET_NAME' # Add your bucket name here
+bucket_name = 'devops-qr-smriti-2025'
 
 @app.post("/generate-qr/")
-async def generate_qr(url: str):
-    # Generate QR Code
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(url)
-    qr.make(fit=True)
-
-    img = qr.make_image(fill_color="black", back_color="white")
-    
-    # Save QR Code to BytesIO object
-    img_byte_arr = BytesIO()
-    img.save(img_byte_arr, format='PNG')
-    img_byte_arr.seek(0)
-
-    # Generate file name for S3
-    file_name = f"qr_codes/{url.split('//')[-1]}.png"
-
+async def generate_qr(url: str = Query(...)):
     try:
+        # Generate QR code
+        qr = qrcode.make(url)
+        buf = BytesIO()
+        qr.save(buf, format="PNG")
+        buf.seek(0)
+
+        # Clean file name
+        safe_name = re.sub(r"[^a-zA-Z0-9_-]", "_", url)
+        file_name = f"qr_codes/{safe_name}.png"
+
         # Upload to S3
-        s3.put_object(Bucket=bucket_name, Key=file_name, Body=img_byte_arr, ContentType='image/png', ACL='public-read')
-        
-        # Generate the S3 URL
-        s3_url = f"https://{bucket_name}.s3.amazonaws.com/{file_name}"
-        return {"qr_code_url": s3_url}
+        s3.put_object(
+            Bucket=bucket_name,
+            Key=file_name,
+            Body=buf,
+            ContentType='image/png',
+            
+        )
+
+        return {"qr_code_url": f"https://{bucket_name}.s3.amazonaws.com/{file_name}"}
     except Exception as e:
+        print(f"QR GENERATION ERROR: {e}")
+        traceback.print_exc() 
         raise HTTPException(status_code=500, detail=str(e))
-    
